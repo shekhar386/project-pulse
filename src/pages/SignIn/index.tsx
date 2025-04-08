ffb5c10ba58c7505d4bc1f5aacaf558eeb3f4d7b
John Doe <johndoe@email.com>
2025-04-08 15:11:45 +0530

    signin page ui

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { CSSProperties, useState } from "react";
 import { setSignInStatus } from "../../app/reducers/authReducer";
 import { useDispatch } from "react-redux";
 
 const SignIn: React.FC = () => {
   const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleOnClick = () => {
     dispatch(setSignInStatus(true));
@@ -13,6 +15,23 @@ const SignIn: React.FC = () => {
   return (
     <div style={styles.backgroundStyle}>
       <div style={styles.mainContainerStyle}>
+        <p>Login to Continue</p>
+        <div style={styles.inputFieldContainerStyle}>
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Enter your email"
+            style={styles.inputFieldStyle}
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Enter your password"
+            style={styles.inputFieldStyle}
+          />
+        </div>
         <button onClick={handleOnClick}>SignIn</button>
       </div>
     </div>
@@ -21,22 +40,47 @@ const SignIn: React.FC = () => {
 
 export default SignIn;
 
-const styles = {
+interface Styles {
+  backgroundStyle: CSSProperties;
+  mainContainerStyle: CSSProperties;
+  inputFieldContainerStyle: CSSProperties;
+  inputFieldStyle: CSSProperties;
+}
+
+const styles: Styles = {
   backgroundStyle: {
-    backgroundColor: "#121212",
+    backgroundImage:
+      "url('https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/wac.92a80da2.svg')",
+    height: "100vh",
+    width: "100vw",
     display: "flex",
-    flex: 1,
-    backgroundImage: "url(https://source.unsplash.com/random)",
     justifyContent: "center",
     alignItems: "center",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    backgroundRepeat: "no-repeat",
   },
   mainContainerStyle: {
-    backgroundColor: "#202020",
-    height: "300px",
-    width: "300px",
+    backgroundColor: "white",
+    boxShadow: "rgba(0, 0, 0, 0.2) 0px 0px 10px",
     display: "flex",
+    flexDirection: "column",
+    padding: "36px",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: "10px",
+    borderRadius: "6px",
+  },
+  inputFieldContainerStyle: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: "24px",
+    marginBottom: "24px",
+  },
+  inputFieldStyle: {
+    padding: "12px",
+    border: "1px solid black",
+    borderRadius: "2px",
+    marginBottom: "12px",
+    width: "16vw",
   },
 };
