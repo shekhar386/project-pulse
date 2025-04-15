2d7102b7effc19c7e6b39d46b8a3cb03fa09f6a7
Shekhar Paliwal <shekharp3000@gmail.com>
2025-04-15 15:24:53 +0530

    sign in page ui complete

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, useState } from "react";
 import { setSignInStatus } from "../../app/reducers/authReducer";
 import { useDispatch } from "react-redux";
+import logo from "../../assets/logo.png";
 
 const SignIn: React.FC = () => {
   const dispatch = useDispatch();
@@ -15,10 +16,14 @@ const SignIn: React.FC = () => {
   return (
     <div style={styles.backgroundStyle}>
       <div style={styles.mainContainerStyle}>
-        <p>Login to Continue</p>
-        <div style={styles.inputFieldContainerStyle}>
+        <div style={styles.imageContainerStyle}>
+          <img src={logo} width={450} height={450} alt="logo" />
+        </div>
+        <h5 style={styles.subHeadingTextStyle}>Sign in to continue</h5>
+        <form style={styles.inputFieldContainerStyle}>
           <input
             type="email"
+            name="email"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
             placeholder="Enter your email"
@@ -26,13 +31,31 @@ const SignIn: React.FC = () => {
           />
           <input
             type="password"
+            name="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             placeholder="Enter your password"
             style={styles.inputFieldStyle}
           />
+          <label style={styles.checkboxContainerStyle}>
+            <input
+              type="checkbox"
+              name="remember"
+              style={styles.checkboxStyle}
+            />
+            <p style={styles.checkboxTextStyle}>Remember me</p>
+          </label>
+        </form>
+        <button style={styles.signInButtonStyle} onClick={handleOnClick}>
+          Sign In
+        </button>
+        {/* separation line */}
+        <div style={styles.separatorStyle} />
+        <div style={styles.otherOptionsContainerStyle}>
+          <p style={styles.linkTextStyle}>Forgot Password?</p>
+          <div style={styles.dotSeparatorStyle} />
+          <p style={styles.linkTextStyle}>Create Account</p>
         </div>
-        <button onClick={handleOnClick}>SignIn</button>
       </div>
     </div>
   );
@@ -45,6 +68,16 @@ interface Styles {
   mainContainerStyle: CSSProperties;
   inputFieldContainerStyle: CSSProperties;
   inputFieldStyle: CSSProperties;
+  imageContainerStyle: CSSProperties;
+  subHeadingTextStyle: CSSProperties;
+  checkboxContainerStyle: CSSProperties;
+  checkboxTextStyle: CSSProperties;
+  signInButtonStyle: CSSProperties;
+  separatorStyle: CSSProperties;
+  otherOptionsContainerStyle: CSSProperties;
+  linkTextStyle: CSSProperties;
+  dotSeparatorStyle: CSSProperties;
+  checkboxStyle: CSSProperties;
 }
 
 const styles: Styles = {
@@ -82,5 +115,65 @@ const styles: Styles = {
     borderRadius: "2px",
     marginBottom: "12px",
     width: "16vw",
+  },
+  imageContainerStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "12vh",
+    width: "16vh",
+  },
+  subHeadingTextStyle: {
+    fontSize: 16,
+  },
+  checkboxContainerStyle: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: "8px",
+    cursor: "pointer",
+  },
+  checkboxTextStyle: {
+    fontSize: 14,
+  },
+  signInButtonStyle: {
+    padding: "12px",
+    backgroundColor: "#0052cc",
+    color: "white",
+    border: "none",
+    borderRadius: "4px",
+    width: "16vw",
+    cursor: "pointer",
+    marginBottom: "12px",
+  },
+  separatorStyle: {
+    width: "80%",
+    height: "2px",
+    backgroundColor: "#ccc",
+    margin: "16px 0",
+  },
+  otherOptionsContainerStyle: {
+    display: "flex",
+    flexDirection: "row",
+    gap: "8px",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  linkTextStyle: {
+    fontSize: 12,
+    color: "#0052cc",
+    cursor: "pointer",
+  },
+  dotSeparatorStyle: {
+    width: "4px",
+    height: "4px",
+    borderRadius: "100%",
+    backgroundColor: "#000",
+  },
+  checkboxStyle: {
+    width: "16px",
+    height: "16px",
+    cursor: "pointer",
   },
 };
