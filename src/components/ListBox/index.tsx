5d3253b324f458a5349705f2278c01fc8ad083ce
Shekhar Paliwal <shekharp3000@gmail.com>
2025-05-20 15:07:28 +0530

    list name editable

--- src/components/ListBox/index.tsx
+++ src/components/ListBox/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { CSSProperties, useState } from "react";
 import "./styles.css";
 import { FaEllipsis } from "react-icons/fa6";
+import EditableText from "../EditableText";
 
 interface ListBoxProps {
   cardArr: unknown[];
@@ -8,11 +9,21 @@ interface ListBoxProps {
 
 const ListBox: React.FC<ListBoxProps> = ({ cardArr }) => {
   const [cardArr0, setCardArr0] = useState<unknown[]>(cardArr);
+  const [listName, setListName] = useState<string>("List Name");
 
   return (
     <div className="listBoxRootContainer">
       <div className="listHeader">
-        <div className="listName">List Name</div>
+        <div className="listName">
+          <EditableText
+            text={listName}
+            textStyles={styles.listNameText}
+            inputStyles={styles.listNameInput}
+            onSave={(value: string): void => {
+              setListName(value);
+            }}
+          />
+        </div>
         <div className="listUtils">
           <FaEllipsis size={12} />
         </div>
@@ -23,3 +34,20 @@ const ListBox: React.FC<ListBoxProps> = ({ cardArr }) => {
 };
 
 export default ListBox;
+
+interface StylesProps {
+  listNameText: CSSProperties;
+  listNameInput: CSSProperties;
+}
+
+const styles: StylesProps = {
+  listNameText: {
+    fontSize: "14px",
+    fontWeight: "700",
+  },
+  listNameInput: {
+    fontSize: "14px",
+    fontWeight: "700",
+    border: "none",
+  },
+};
