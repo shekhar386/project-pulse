86810e924bb2365789c9c4d8d6dc8cac237e7113
Shekhar Paliwal <shekharp3000@gmail.com>
2025-05-20 12:46:49 +0530

    search input and filter content update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHouse, FaCircleUser } from "react-icons/fa6";
 import TextInput from "../../components/TextInput";
 import "./styles.css";
 
 const Home: React.FC = () => {
+  const [searchInputValue, setSearchInputValue] = useState<string>("");
+
   return (
     <div className="rootContainer">
       <div className="headContainer">
@@ -14,13 +16,22 @@ const Home: React.FC = () => {
           <TextInput
             placeholder="Search"
             type="text"
-            value=""
-            onChange={() => {}}
+            value={searchInputValue}
+            onChange={(value: string): void => {
+              setSearchInputValue(value);
+            }}
           />
+          <button className="searchButton" onClick={() => {}}>
+            Search
+          </button>
         </div>
         <div className="profileContainer">
           <FaCircleUser size={24} className="profileIcon" />
         </div>
+      </div>
+      <div className="filterContainer">
+        <div className="projectName">Project Name</div>
+        <div className="filterUtils">Filters</div>
       </div>
       <div className="contentContainer">Block 3</div>
     </div>
